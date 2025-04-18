6224389b2aad03af2bac89934445af5ec44d0828
WhittJS <whittsanders@gmail.com>
2025-04-18 17:58:15 -0400

    response framework

--- prompt-engineering.py
+++ prompt-engineering.py
@@ -1,18 +1,45 @@
 import os
 from openai import OpenAI
-
-# Create your PAT token by following instructions here:
-# https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens
-# grab the token from the token.txt file
-with open("token.txt", "r") as f:
-    os.environ["GITHUB_TOKEN"] = f.read().strip()
+import pandas as pd
+from tqdm import tqdm
 
 
-# To authenticate with the model you will need to generate a personal access token (PAT) in your GitHub settings.
-client = OpenAI(
-    base_url="https://models.inference.ai.azure.com",
-    api_key=os.environ["GITHUB_TOKEN"],
-)
+def get_response(max_tokens, model, temp, prompt, role=""):
+    response = client.chat.completions.create(
+        model=model,
+        messages=[{"role": "user", "content": f"{prompt}"},] if role == ""
+        else [{"role": "system", "content": f"{role}"},{"role": "user", "content": f"{prompt}"},],
+        max_tokens=max_tokens,
+        temperature=temp,
+        )
+    return response.choices[0].message.content
 
-# List of available models
-model_choices = ["gpt-4o", "gpt-4.1"]
+
+def get_all_responses(max_tokens, temp, prompt, role):
+    response_list = []
+    for model in model_choices:
+        response_list.append(get_response(max_tokens, model, temp, prompt))
+        response_list.append(get_response(max_tokens, model, temp, prompt, role))
+    return response_list
+
+if __name__ == "__main__":
+    # Create your PAT token by following instructions here:
+    # https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens
+    # grab the token from the token.txt file
+    with open("token.txt", "r") as f:
+        os.environ["GITHUB_TOKEN"] = f.read().strip()
+
+
+    # To authenticate with the model you will need to generate a personal access token (PAT) in your GitHub settings.
+    client = OpenAI(
+        base_url="https://models.inference.ai.azure.com",
+        api_key=os.environ["GITHUB_TOKEN"],
+    )
+
+    # List of available models
+    model_choices = ["gpt-4o", "gpt-4.1"]
+
+    df = pd.read_csv("prompts.csv")
+        
+    for index, row in tqdm(df.iterrows()):
+        print(get_all_responses(1024, 1, row['zero-shot'], row['roleplay-role']))
